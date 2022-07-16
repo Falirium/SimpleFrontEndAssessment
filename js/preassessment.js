7cfb124fb4ffb34a2cb34cc8af6d9a97497db85d
falirium <mafarfaoua@insea.ac.ma>
2022-07-16 03:16:04 +0100

    Fix errors about nesting new criterias for new categories

--- js/preassessment.js
+++ js/preassessment.js
@@ -1,12 +1,13 @@
 const textField = document.querySelector("#textField");
 const inputId = document.querySelector("#input-id");
 const btnAddNewCategory = document.querySelector("#btn-add-category");
+const form_modal = document.querySelector("#form-modal");
 
 let compteurHeaders = 1;
 
 
 function parseToTable(json) {
-    
+
     // const table = document.getElementsByClassName('table table-striped');
     // console.log(table);
     let tableHeader = document.querySelector("#thead");
@@ -35,55 +36,55 @@ function parseToTable(json) {
     // ]
 
     // EXTRACT VALUE FOR HTML HEADER. 
-    
-        var col = [];
-        for (var i = 0; i < json.length; i++) {
-            for (var key in json[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
+
+    var col = [];
+    for (var i = 0; i < json.length; i++) {
+        for (var key in json[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
             }
         }
+    }
 
-        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
 
-        var tr = tableHeader.insertRow(-1);                   // TABLE ROW.
+    var tr = tableHeader.insertRow(-1);                   // TABLE ROW.
 
-        for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");      // TABLE HEADER.
-            th.innerHTML = col[i];
-            tr.appendChild(th);
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0; i < json.length; i++) {
+
+        tr = tableBody.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = json[i][col[j]];
         }
-
-        // ADD JSON DATA TO THE TABLE AS ROWS.
-        for (var i = 0; i < json.length; i++) {
-
-            tr = tableBody.insertRow(-1);
-
-            for (var j = 0; j < col.length; j++) {
-                var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = json[i][col[j]];
-            }
-        }
+    }
 
 
-        // Hide upload btn and show classify btn
+    // Hide upload btn and show classify btn
 
-        let btn_show = document.querySelector("#btn-show");
-        let btn_hide = document.querySelector("#btn-classify");
+    let btn_show = document.querySelector("#btn-show");
+    let btn_hide = document.querySelector("#btn-classify");
 
-        btn_show.className = "btn btn-primary hide";
-        btn_hide.className = "btn btn-primary show";
-        
+    btn_show.className = "btn btn-primary hide";
+    btn_hide.className = "btn btn-primary show";
+
 
 
 }
 
 async function fetchEmployeesData() {
-    
-    const id = inputId.value; 
 
-    let url ="http://localhost:8080/preassessment/api/v1/2json/" + id;
+    const id = inputId.value;
+
+    let url = "http://localhost:8080/preassessment/api/v1/2json/" + id;
     try {
         let res = await fetch(url);
         let response = await res.json();
@@ -96,24 +97,24 @@ async function fetchEmployeesData() {
 
 async function postExcelFile(file) {
 
-    let url ="http://localhost:8080/preassessment/api/v1/upload"
+    let url = "http://localhost:8080/preassessment/api/v1/upload"
 
     console.log(file);
     fetch(url, { // Your POST endpoint
         method: 'POST',
         headers: {
-        // Content-Type may need to be completely **omitted**
-        // or you may need something
-        "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=UTF-8"
+            // Content-Type may need to be completely **omitted**
+            // or you may need something
+            "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=UTF-8"
         },
         body: file // This is your file object
     }).then(
         response => {
             let json = response.json();
-            
 
-            textField.innerHTML= json.message;
-        
+
+            textField.innerHTML = json.message;
+
         } // if the response is a JSON object
     ).then(
         success => console.log(success) // Handle the success response object
@@ -121,7 +122,7 @@ async function postExcelFile(file) {
         error => console.log(error) // Handle the error response object
     );
 
-    
+
 }
 
 // Select your input type file and store it in a variable
@@ -193,74 +194,88 @@ populateWithClassificationColumns();
 
 
 
-function populateWithClassificationColumns(select) {
-    
-    let allSelectFields = document.querySelectorAll("#select-classification");
+function populateWithClassificationColumns() {
 
-    let lastSelectField = allSelectFields[allSelectFields.length - 1];
-    classificationColumns.map( e => {
-        var option = new Option(e,"");
+    let lastCriteria = lastCriteriaContainer();
+
+    let lastSelectField = lastCriteria.querySelector("#select-classification");
+
+    classificationColumns.map(e => {
+        var option = new Option(e, "");
         lastSelectField.appendChild(option);
     })
 }
 
 
 function addNewCategory() {
-    let parent = document.querySelector(".model-form-container").parentElement;
-    let div_container = document.createElement("div");
-    div_container.className = "my-4 model-form-container";
+    let lastCategory = lastCategoryContainer();
+    let parent = lastCategory.parentElement;
+    let newCategory = document.createElement("div");
 
-    parent.appendChild(div_container)
-
-    div_container.innerHTML = `<h2 class="text-center" id="header-category">Catégorie numéro </h2><label for="catgory-name" class="col-form-label">Nom de la catégorie</label>
-    <input type="text" class="form-control" id="catgory-name">
-
-    <label for="select-classification" class="col-form-label">Choisissez votre critière de classification</label>
-    <select class="form-select" aria-label="Default select example" id="select-classification">
-        
-    </select>
-    <div class="d-flex w-100 justify-content-around">
-        <div >
-            <label for="min-input">Valeur Min :</label>
-            <input id="min-input" type="text" class="form-control mt-2">
-        </div>
-        <div>
-            <label for="max-input">Valeur Max :</label>
-            <input id="max-input" type="text" class="form-control mt-2">
-        </div>
-    </div> <hr>`;
-
+    parent.appendChild(newCategory);
+    
     
 
+    newCategory.outerHTML = `
+    <div class="my-4 category-container">
+                                    <h2 class="text-center" id="header-category">Catégorie numéro 1</h2>
+                                    <label for="catgory-name" class="col-form-label">Nom de la catégorie</label>
+                                    <input type="text" class="form-control" id="catgory-name">
 
-    console.log(div_container);
+                                    <div class="criteria-container mb-3">
+                                        <label for="select-classification" class="col-form-label">Choisissez votre critière de classification</label>
+                                        <select class="form-select" aria-label="Default select example" id="select-classification">
+                                        
+                                        </select>
+
+                                        <div class="d-flex w-100 justify-content-around">
+                                            <div >
+                                                <label for="min-input">Valeur Min :</label>
+                                                <input id="min-input" type="text" class="form-control mt-2">
+                                            </div>
+                                            <div>
+                                                <label for="max-input">Valeur Max :</label>
+                                                <input id="max-input" type="text" class="form-control mt-2">
+                                            </div>
+                                        </div>
+                                    </div> 
+
+                                </div> <hr>`;
+
+    populateWithClassificationColumns();
+
+
+
+
+    //console.log(div_container);
 }
 function addNewCriteria() {
-    let allDivs = document.querySelectorAll(".criteria-container");
-    console.log(allDivs);
-    let parent = allDivs[allDivs.length - 1];
-    let div_container = document.createElement("div");
-    div_container.className = "criteria-container mb-3";
-
-    parent.appendChild(div_container);
-
-    div_container.outerHTML =`<div class="criteria-container mb-3">
-    <label for="select-classification" class="col-form-label">Choisissez votre critière de classification</label>
-    <select class="form-select" aria-label="Default select example" id="select-classification">
+    let lastCriteria = lastCriteriaContainer();
+    let parent = lastCriteria.parentElement;
+    let newCriteria = document.createElement("div");
     
-    </select>
 
-    <div class="d-flex w-100 justify-content-around">
-        <div >
-            <label for="min-input">Valeur Min :</label>
-            <input id="min-input" type="text" class="form-control mt-2">
-        </div>
-        <div>
-            <label for="max-input">Valeur Max :</label>
-            <input id="max-input" type="text" class="form-control mt-2">
+    parent.appendChild(newCriteria);
+
+    newCriteria.outerHTML = `
+    <div class="criteria-container mb-3">
+        <label for="select-classification" class="col-form-label">Choisissez votre critière de classification</label>
+        <select class="form-select" aria-label="Default select example" id="select-classification">
+        
+        </select>
+
+        <div class="d-flex w-100 justify-content-around">
+            <div >
+                <label for="min-input">Valeur Min :</label>
+                <input id="min-input" type="text" class="form-control mt-2">
+            </div>
+            <div>
+                <label for="max-input">Valeur Max :</label>
+                <input id="max-input" type="text" class="form-control mt-2">
+            </div>
         </div>
     </div>
-</div> `;
+    `;
 
     populateWithClassificationColumns();
 
@@ -268,16 +283,34 @@ function addNewCriteria() {
 }
 
 btnAddNewCategory.addEventListener("click", () => {
-    
+
 
     compteurHeaders++;
     let headersCategories = document.querySelectorAll("#header-category");
-    console.log(headersCategories);
+    //console.log(headersCategories);
 
     let lastHeaderCategory = headersCategories[headersCategories.length - 1];
 
     lastHeaderCategory.innerHTML = "Catégorie numéro " + compteurHeaders;
 })
+
+
+function lastCriteriaContainer() {
+    let lastCategory = lastCategoryContainer();
+    let allCriteriasContainers = lastCategory.querySelectorAll(".criteria-container");
+
+    let lastCriteria = allCriteriasContainers[allCriteriasContainers.length - 1];
+
+    return lastCriteria;
+}
+
+function lastCategoryContainer() {
+    let allCategoriesContainers = form_modal.querySelectorAll(".category-container");
+
+    let lastCategory = allCategoriesContainers[allCategoriesContainers.length - 1];
+
+    return lastCategory;
+}
 
 function getFilters() {
 
